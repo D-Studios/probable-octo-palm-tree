0e37dd726191fab5fac739986b8cd33955b1d129
Devang Bhatnagar <gamefun7102@gmail.com>
2024-10-03 11:20:39 -0700

    Working on Task2.py

---
+++ Task2.py
@@ -0,0 +1,131 @@
+from Crypto.Cipher import AES
+import base64
+import random
+import string
+import urllib.parse
+
+LAST_BYTE = -1
+MINIMUM_PAD_SIZE = 1
+LAST_INDEX = -1
+SECOND_LAST_BYTE = -2
+WRONG_PADDING_NUMBER = 255
+BLOCKSIZE = 16
+
+
+def xorFunction(input_bytes, key_bytes):
+    j=0 
+    last_index = len(key_bytes)-1
+    bytes_list = []
+    for i in range(0, len(input_bytes)):
+        if j>last_index:
+            j=0
+        bytes_list.append(input_bytes[i] ^ key_bytes[j])
+        j+=1
+    result_bytes = bytes_list
+    return result_bytes
+
+def pad(plaintextMessage):
+	padSize = BLOCKSIZE - (len(plaintextMessage) % BLOCKSIZE)
+	padding = bytes([padSize] * padSize)
+	data = plaintextMessage + padding 
+	return data
+
+def unpad(data):
+	data_length = len(data)
+	last_data_index = data_length + LAST_INDEX
+	pad_index = last_data_index
+	padSize = data[LAST_BYTE]
+	if data_length%BLOCKSIZE != 0:
+		raise ValueError("Pad size is not multiple of blocksize.")
+	if padSize < MINIMUM_PAD_SIZE or padSize > data_length:
+		raise ValueError("Invalid padding size")
+	i = 0
+	while True:
+		if i>=padSize:
+			break
+		pad_byte = data[pad_index]
+		if pad_byte != padSize:
+			raise ValueError("Invalid padding byte")
+		i+=1
+		pad_index-=1
+	return data[0:data_length-padSize]
+
+def cbc_encrypt(plaintext, key, initializationVector):
+	data = pad(plaintext)
+	cipher = AES.new(key, AES.MODE_ECB)
+	ciphertext = b""
+	prev_block = initializationVector
+	for i in range(0, len(data), BLOCKSIZE):
+		block = data[i : i+BLOCKSIZE]
+		xored = bytes(xorFunction(block, prev_block))
+		encrypted_block = cipher.encrypt(xored)
+		ciphertext += encrypted_block
+		prev_block = encrypted_block
+	return ciphertext
+
+def cbc_decrypt(ciphertext, key, initializationVector):
+	cipher = AES.new(key, AES.MODE_ECB)
+	if len(ciphertext)%BLOCKSIZE != 0:
+		raise ValueError("Decrypted ciphertext not multiple of block size.")
+	
+	decryptedCipherText = b""
+	prev_block = initializationVector
+
+	for i in range(0, len(ciphertext), BLOCKSIZE):
+		block = ciphertext[i : i+BLOCKSIZE]
+		decrypted_block = cipher.decrypt(block)
+		decryptedCipherText += bytes(xorFunction(decrypted_block, prev_block))
+		prev_block = block
+	plaintext = unpad(decryptedCipherText)
+	return plaintext
+
+def generate_random_key(length) :
+	characters = string.ascii_letters + string.digits
+	random_key = ''.join(random.choice(characters) for i in range(length))
+	return random_key.encode('utf-8')
+
+def generate_random_iv(length):
+	random_iv = bytes(random.getrandbits(8) for i in range(length))
+	return random_iv
+
+def custom_url_encode(string, encode_chars):
+	encode_set = set(encode_chars)
+	encoded_string = ''
+	for char in string:
+		if char in encode_set:
+			encoded_string += urllib.parse.quote(char)
+		else:
+			encoded_string += char 
+	return encoded_string 
+
+	
+def submit():
+	random.seed(42)
+	key = generate_random_key(16)
+	random_iv = generate_random_iv(16)
+	string = input("Enter arbitrary string : ")
+	plaintext = "userid=456;userdata=" 
+	plaintext += string 
+	plaintext += ";session-id=31337"
+	plaintext = custom_url_encode(plaintext, [';', '='])
+	ciphertext = cbc_encrypt(plaintext, )
+
+def main():
+
+	submit()
+	# with open(plainTextFile, 'rb') as file:
+	# 	plaintext = file.read()
+
+	# ciphertext = cbc_encrypt(plaintext, key, random_iv)
+	# checker = cbc_decrypt(ciphertext, key, random_iv)
+
+	# if(checker != plaintext):
+	# 	raise ValueError("Something is wrong with encryption/decryption.")
+	# 	return
+
+	# with open(outputFile, 'wb') as file:
+	# 	file.write(ciphertext)
+
+if __name__ == '__main__':
+	main()
+
